c42e5b4856ec640a4312198e27c37b4c7e1769b8
kawnayeen <kawnayeen.08@gmail.com>
2017-11-30 10:09:33 +0600

    done upto displaying eye

---
+++ Faceit/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Kawnayeen on 11/28/17.
+//  Copyright © 2017 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
